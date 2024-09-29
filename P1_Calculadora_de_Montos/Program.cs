f18f1711966c094f8b18179a28c94bc82910995a
Angie Campos <125337384+jumAngie@users.noreply.github.com>
2024-09-29 13:22:57 -0600

    P1

---
+++ P1_Calculadora_de_Montos/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Angie Yahaira Campos
+// 20222000215
+
+namespace P1_Calculadora_de_Montos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" ** CALCULADORA DE MONTOS ** ");
+            Console.WriteLine(" ");
+
+            double precio = 0;
+            int cantidad = 0;
+
+            Console.Write("Ingrese el precio del producto  L. ");
+            precio = double.Parse(Console.ReadLine());
+            Console.Write("Ingrese la cantidad seleccionada: ");
+            cantidad = int.Parse(Console.ReadLine());
+
+
+            double monto = precio * cantidad;
+
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("MONTO A CANCELAR: L. " + monto.ToString("0.00"));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ P2_Promedios_De_Nota/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P2_Promedios_De_Nota
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

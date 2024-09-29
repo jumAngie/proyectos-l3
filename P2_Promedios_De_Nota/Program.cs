3d157b515a1c25579da56aa2494cf80308dd91b9
Angie Campos <125337384+jumAngie@users.noreply.github.com>
2024-09-29 13:32:58 -0600

    P2

--- P2_Promedios_De_Nota/Program.cs
+++ P2_Promedios_De_Nota/Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
 
+//Angie Yahaira Campos Arias
+//20222000215
+
 namespace P2_Promedios_De_Nota
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(" ** PROMEDIO DE NOTAS ** ");
+            Console.WriteLine(" ");
+
+            string alumno;
+            int nota1, nota2, nota3;
+
+            Console.Write("Ingrese nombre del alumno: ");
+            alumno = Console.ReadLine();
+            Console.Write("Ingrese la nota 1: ");
+            nota1 = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese la nota 2: ");
+            nota2 = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese la nota 3: ");
+            nota3 = int.Parse(Console.ReadLine());
+
+            double promedio = (nota1 + nota2 + nota3) / 3;
+
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine("EL PROMEDIO: " + promedio.ToString("0.00") + "%");
+
+            Console.ReadKey();
+
+
         }
     }
 }

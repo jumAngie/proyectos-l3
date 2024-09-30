d2e416c09bd1e0732dd1c8111faa07630fb38b92
Angie Campos <125337384+jumAngie@users.noreply.github.com>
2024-09-30 13:49:18 -0600

    P7

---
+++ P7_Control_De_Medidas/frmMedidas.Designer.cs
@@ -0,0 +1,178 @@
+﻿
+namespace P7_Control_De_Medidas
+{
+    partial class frmMedidas
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMedidas));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtMetros = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lstR = new System.Windows.Forms.ListBox();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::P7_Control_De_Medidas.Properties.Resources.tumblr_79cf132d818b744132b062249579ca6a_43dde9db_2048;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(607, 163);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Sitka Subheading", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DeepPink;
+            this.label1.Location = new System.Drawing.Point(204, 178);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(245, 30);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "CONTROL DE MEDIDAS";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Sitka Subheading", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DeepPink;
+            this.label2.Location = new System.Drawing.Point(12, 228);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(222, 23);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "INGRESE VALOR EN METROS";
+            // 
+            // txtMetros
+            // 
+            this.txtMetros.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMetros.ForeColor = System.Drawing.Color.DeepPink;
+            this.txtMetros.Location = new System.Drawing.Point(237, 227);
+            this.txtMetros.Name = "txtMetros";
+            this.txtMetros.Size = new System.Drawing.Size(110, 26);
+            this.txtMetros.TabIndex = 3;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.BackColor = System.Drawing.Color.DeepPink;
+            this.btnCalcular.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCalcular.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCalcular.Font = new System.Drawing.Font("Sitka Small", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.ForeColor = System.Drawing.Color.Moccasin;
+            this.btnCalcular.Location = new System.Drawing.Point(353, 227);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(116, 28);
+            this.btnCalcular.TabIndex = 4;
+            this.btnCalcular.Text = "CALCULAR";
+            this.btnCalcular.UseVisualStyleBackColor = false;
+            this.btnCalcular.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lstR
+            // 
+            this.lstR.BackColor = System.Drawing.Color.PapayaWhip;
+            this.lstR.Font = new System.Drawing.Font("Sitka Text", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstR.ForeColor = System.Drawing.Color.DeepPink;
+            this.lstR.FormattingEnabled = true;
+            this.lstR.ItemHeight = 19;
+            this.lstR.Location = new System.Drawing.Point(16, 261);
+            this.lstR.Name = "lstR";
+            this.lstR.Size = new System.Drawing.Size(603, 137);
+            this.lstR.TabIndex = 5;
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.BackColor = System.Drawing.Color.DeepPink;
+            this.btnLimpiar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnLimpiar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnLimpiar.Font = new System.Drawing.Font("Sitka Heading", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpiar.ForeColor = System.Drawing.Color.Moccasin;
+            this.btnLimpiar.Location = new System.Drawing.Point(16, 408);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(100, 38);
+            this.btnLimpiar.TabIndex = 6;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = false;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.BackColor = System.Drawing.Color.DeepPink;
+            this.btnSalir.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSalir.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSalir.Font = new System.Drawing.Font("Sitka Heading", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.ForeColor = System.Drawing.Color.Moccasin;
+            this.btnSalir.Location = new System.Drawing.Point(122, 408);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(100, 38);
+            this.btnSalir.TabIndex = 7;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = false;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // frmMedidas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Moccasin;
+            this.ClientSize = new System.Drawing.Size(631, 458);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.lstR);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtMetros);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmMedidas";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Control de Medidas";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtMetros;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.ListBox lstR;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnSalir;
+    }
+}
+
---
+++ P7_Control_De_Medidas/frmMedidas.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P7_Control_De_Medidas
+{
+    public partial class frmMedidas : Form
+    {
+        public frmMedidas()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Capturando el valor en metros
+            double metros = double.Parse(txtMetros.Text);
+
+            // Realizando conversiones
+            double centimetros = metros * 100;
+            double pulgadas = centimetros / 2.54;
+            double pies = pulgadas / 12;
+            double yardas = pies / 3;
+
+            // Mostrar los resultados
+            lstR.Items.Add("** RESUMEN DE CONVERSIOES ** ");
+            lstR.Items.Add("MEDIDA EN METROS: " + metros.ToString("0.00"));
+            lstR.Items.Add("------------------------------------------");
+            lstR.Items.Add("MEDIDA EN CENTIMETROS:  " + centimetros.ToString("0.00"));
+            lstR.Items.Add("MEDIDA EN PULGADAS:     " + pulgadas.ToString("0.00"));
+            lstR.Items.Add("MEDIDA EN PIES:         " + pies.ToString("0.00"));
+            lstR.Items.Add("MEDIDA EN YARDAS:       " + yardas.ToString("0.00"));
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txtMetros.Clear();
+            lstR.Items.Clear();
+            txtMetros.Focus();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            DialogResult respuesta = MessageBox.Show("Esta seguro de salir?", 
+                                                    "Medidas",
+                                                    MessageBoxButtons.YesNo,
+                                                    MessageBoxIcon.Information);
+
+            if (respuesta == DialogResult.Yes) this.Close();
+        }
+    }
+}

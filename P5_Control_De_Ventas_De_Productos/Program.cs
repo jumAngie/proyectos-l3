b230a888b4d1ba88e6c79f04834ffb43f4ccf45d
Angie Campos <125337384+jumAngie@users.noreply.github.com>
2024-09-30 12:37:30 -0600

    P5

---
+++ P5_Control_De_Ventas_De_Productos/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace P5_Control_De_Ventas_De_Productos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" ** CONTRO DE VENTA DE PRODUCTOS ** ");
+            Console.WriteLine(" ");
+
+            double precio;
+            int cantidad;
+
+            Console.Write("Ingrese precio del producto: HNL. ");
+            precio = double.Parse(Console.ReadLine());
+            Console.Write("Ingrese cantidad comprada: ");
+            cantidad = int.Parse(Console.ReadLine());
+
+            double importe = cantidad * precio;
+            double descuento = 11 / 100.0 * importe;
+            double total = importe - descuento;
+
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine("IMPORTE TOTAL:   HNL. " + importe.ToString("0.00"));
+            Console.WriteLine("DESCUENTO :      HNL. " + descuento.ToString("0.00"));
+            Console.WriteLine("TOTAL A PAGAR:   HNL. " + total.ToString("0.00"));
+
+            Console.ReadKey();
+        }
+    }
+}

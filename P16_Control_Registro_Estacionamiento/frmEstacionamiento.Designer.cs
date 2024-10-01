72ed088015785594d02e7f7d5ae6b99b62bf8cc4
Angie Campos <125337384+jumAngie@users.noreply.github.com>
2024-10-01 14:39:53 -0600

    P16

---
+++ P16_Control_Registro_Estacionamiento/frmEstacionamiento.Designer.cs
@@ -0,0 +1,351 @@
+﻿namespace P16_Control_Registro_Estacionamiento
+{
+    partial class frmEstacionamiento
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPlaca = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblCosto = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblFecha = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.btnRegistrar = new System.Windows.Forms.Button();
+            this.lvRegistro = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.txtHoraInicio = new System.Windows.Forms.MaskedTextBox();
+            this.txtHoraSalida = new System.Windows.Forms.MaskedTextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS Gothic", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkGreen;
+            this.label1.Location = new System.Drawing.Point(82, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(621, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CONTROL DE REGISTRO DE ESTACIONAMIENTO";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.DarkGreen;
+            this.label2.Font = new System.Drawing.Font("Raleway SemiBold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Honeydew;
+            this.label2.Location = new System.Drawing.Point(41, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 26);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "PLACA";
+            // 
+            // txtPlaca
+            // 
+            this.txtPlaca.Font = new System.Drawing.Font("Raleway SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPlaca.Location = new System.Drawing.Point(121, 61);
+            this.txtPlaca.MaxLength = 5;
+            this.txtPlaca.Name = "txtPlaca";
+            this.txtPlaca.Size = new System.Drawing.Size(142, 26);
+            this.txtPlaca.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Honeydew;
+            this.label3.Font = new System.Drawing.Font("Raleway SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.DarkGreen;
+            this.label3.Location = new System.Drawing.Point(299, 86);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(178, 22);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "COSTO DEL DIA (HNL.)\r\n";
+            // 
+            // lblCosto
+            // 
+            this.lblCosto.AutoSize = true;
+            this.lblCosto.BackColor = System.Drawing.Color.DarkGreen;
+            this.lblCosto.Font = new System.Drawing.Font("Raleway", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCosto.ForeColor = System.Drawing.Color.Honeydew;
+            this.lblCosto.Location = new System.Drawing.Point(328, 118);
+            this.lblCosto.Name = "lblCosto";
+            this.lblCosto.Size = new System.Drawing.Size(113, 34);
+            this.lblCosto.TabIndex = 4;
+            this.lblCosto.Text = "lblCosto";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtHoraSalida);
+            this.groupBox1.Controls.Add(this.txtHoraInicio);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.lblFecha);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Font = new System.Drawing.Font("Raleway Medium", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(507, 61);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(252, 164);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "FECHA Y HORA";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.DarkGreen;
+            this.label5.Font = new System.Drawing.Font("Raleway SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Honeydew;
+            this.label5.Location = new System.Drawing.Point(30, 35);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(63, 22);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "FECHA";
+            // 
+            // lblFecha
+            // 
+            this.lblFecha.AutoSize = true;
+            this.lblFecha.BackColor = System.Drawing.Color.Honeydew;
+            this.lblFecha.Font = new System.Drawing.Font("Raleway SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFecha.ForeColor = System.Drawing.Color.DarkGreen;
+            this.lblFecha.Location = new System.Drawing.Point(127, 35);
+            this.lblFecha.Name = "lblFecha";
+            this.lblFecha.Size = new System.Drawing.Size(63, 22);
+            this.lblFecha.TabIndex = 7;
+            this.lblFecha.Text = "FECHA";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Honeydew;
+            this.label7.Font = new System.Drawing.Font("Raleway SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.DarkGreen;
+            this.label7.Location = new System.Drawing.Point(18, 79);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(107, 22);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "HORA INICIO";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Honeydew;
+            this.label8.Font = new System.Drawing.Font("Raleway SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.DarkGreen;
+            this.label8.Location = new System.Drawing.Point(17, 121);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(108, 22);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "HORA SALID.";
+            // 
+            // btnRegistrar
+            // 
+            this.btnRegistrar.BackColor = System.Drawing.Color.DarkGreen;
+            this.btnRegistrar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnRegistrar.Font = new System.Drawing.Font("Raleway", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegistrar.ForeColor = System.Drawing.Color.Honeydew;
+            this.btnRegistrar.Location = new System.Drawing.Point(46, 182);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(160, 38);
+            this.btnRegistrar.TabIndex = 6;
+            this.btnRegistrar.Text = "REGISTRAR";
+            this.btnRegistrar.UseVisualStyleBackColor = false;
+            this.btnRegistrar.Click += new System.EventHandler(this.btnRegistrar_Click);
+            // 
+            // lvRegistro
+            // 
+            this.lvRegistro.BackColor = System.Drawing.Color.DarkGreen;
+            this.lvRegistro.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6,
+            this.columnHeader7});
+            this.lvRegistro.ForeColor = System.Drawing.Color.Honeydew;
+            this.lvRegistro.GridLines = true;
+            this.lvRegistro.HideSelection = false;
+            this.lvRegistro.Location = new System.Drawing.Point(46, 231);
+            this.lvRegistro.Name = "lvRegistro";
+            this.lvRegistro.Size = new System.Drawing.Size(713, 184);
+            this.lvRegistro.TabIndex = 7;
+            this.lvRegistro.UseCompatibleStateImageBehavior = false;
+            this.lvRegistro.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "PLACA";
+            this.columnHeader1.Width = 100;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "FECHA";
+            this.columnHeader2.Width = 110;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "HORA INICIO";
+            this.columnHeader3.Width = 110;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "HORA FIN";
+            this.columnHeader4.Width = 110;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "HORAS";
+            this.columnHeader5.Width = 90;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "TARIFA";
+            this.columnHeader6.Width = 100;
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "IMPORTE";
+            this.columnHeader7.Width = 90;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.BackColor = System.Drawing.Color.DarkGreen;
+            this.btnSalir.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSalir.Font = new System.Drawing.Font("Raleway", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.ForeColor = System.Drawing.Color.Honeydew;
+            this.btnSalir.Location = new System.Drawing.Point(46, 422);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.btnSalir.Size = new System.Drawing.Size(130, 38);
+            this.btnSalir.TabIndex = 8;
+            this.btnSalir.Text = "SALIR";
+            this.btnSalir.UseVisualStyleBackColor = false;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.BackColor = System.Drawing.Color.Honeydew;
+            this.btnCancelar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCancelar.Font = new System.Drawing.Font("Raleway", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelar.ForeColor = System.Drawing.Color.DarkGreen;
+            this.btnCancelar.Location = new System.Drawing.Point(182, 421);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(130, 38);
+            this.btnCancelar.TabIndex = 9;
+            this.btnCancelar.Text = "CANCELAR";
+            this.btnCancelar.UseVisualStyleBackColor = false;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // txtHoraInicio
+            // 
+            this.txtHoraInicio.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtHoraInicio.ForeColor = System.Drawing.Color.DarkGreen;
+            this.txtHoraInicio.Location = new System.Drawing.Point(131, 75);
+            this.txtHoraInicio.Mask = "00:00";
+            this.txtHoraInicio.Name = "txtHoraInicio";
+            this.txtHoraInicio.Size = new System.Drawing.Size(100, 26);
+            this.txtHoraInicio.TabIndex = 10;
+            this.txtHoraInicio.ValidatingType = typeof(System.DateTime);
+            // 
+            // txtHoraSalida
+            // 
+            this.txtHoraSalida.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtHoraSalida.ForeColor = System.Drawing.Color.DarkGreen;
+            this.txtHoraSalida.Location = new System.Drawing.Point(131, 117);
+            this.txtHoraSalida.Mask = "00:00";
+            this.txtHoraSalida.Name = "txtHoraSalida";
+            this.txtHoraSalida.Size = new System.Drawing.Size(100, 26);
+            this.txtHoraSalida.TabIndex = 11;
+            this.txtHoraSalida.ValidatingType = typeof(System.DateTime);
+            // 
+            // frmEstacionamiento
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Honeydew;
+            this.ClientSize = new System.Drawing.Size(800, 472);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.lvRegistro);
+            this.Controls.Add(this.btnRegistrar);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lblCosto);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtPlaca);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmEstacionamiento";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmEstacionamiento_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPlaca;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblCosto;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lblFecha;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnRegistrar;
+        private System.Windows.Forms.ListView lvRegistro;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.MaskedTextBox txtHoraSalida;
+        private System.Windows.Forms.MaskedTextBox txtHoraInicio;
+    }
+}
+
---
+++ P16_Control_Registro_Estacionamiento/frmEstacionamiento.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//Angie Yahaira Campos Arias
+// 20222000215
+
+namespace P16_Control_Registro_Estacionamiento
+{
+    public partial class frmEstacionamiento : Form
+    {
+        string dia;
+        public frmEstacionamiento()
+        {
+            InitializeComponent();
+        }
+
+        private void frmEstacionamiento_Load(object sender, EventArgs e)
+        {
+            lblFecha.Text = DateTime.Now.ToShortDateString();
+            DateTime fecha = DateTime.Parse(lblFecha.Text);
+            dia = fecha.ToString("dddd");
+
+            double costo = 0;
+            switch (dia)
+            {
+                case "domingo": costo = 2; break;
+                case "lunes": 
+                case "martes":
+                case "miercoles": 
+                case "jueves": costo = 4; break;
+                case "viernes":
+                case "sábado": costo = 7; break;
+            }
+
+            lblCosto.Text = costo.ToString("0.00");
+        }
+
+        private void btnRegistrar_Click(object sender, EventArgs e)
+        {
+            string placa = txtPlaca.Text;
+            double costo = double.Parse(lblCosto.Text);
+            DateTime fecha = DateTime.Parse(lblFecha.Text);
+            DateTime horaInicio = DateTime.Parse(txtHoraInicio.Text);
+            DateTime horaFin = DateTime.Parse(txtHoraSalida.Text);
+
+            TimeSpan hora = horaFin - horaInicio;
+
+            double importe = costo * (hora.TotalHours);
+
+            ListViewItem fila = new ListViewItem(placa);
+            fila.SubItems.Add(fecha.ToString("d"));
+            fila.SubItems.Add(horaInicio.ToString("t"));
+            fila.SubItems.Add(horaFin.ToString("t"));
+            fila.SubItems.Add(hora.TotalHours.ToString("0"));
+            fila.SubItems.Add(costo.ToString("C"));
+            fila.SubItems.Add(importe.ToString("C"));
+            lvRegistro.Items.Add(fila);
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            txtPlaca.Clear();
+            txtHoraInicio.Clear();
+            txtHoraSalida.Clear();
+            txtPlaca.Focus();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            DialogResult r = MessageBox.Show("Esta seguro de salir?", "Estacionamiento",
+                                            MessageBoxButtons.YesNo, MessageBoxIcon.Question);  
+            
+            if (r == DialogResult.Yes) this.Close();
+        }
+    }
+}

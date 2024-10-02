8db690c6695cc8fe0da0eef0752a19fb339d8fb0
Angie Campos <125337384+jumAngie@users.noreply.github.com>
2024-10-01 18:07:57 -0600

    P18

---
+++ P18_Planilla_Sueldo/frmPlanilla.Designer.cs
@@ -0,0 +1,354 @@
+﻿namespace P18_Planilla_Sueldo
+{
+    partial class frmPlanilla
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblFecha = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtDNI = new System.Windows.Forms.TextBox();
+            this.txtEmpleado = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cboCategoria = new System.Windows.Forms.ComboBox();
+            this.lblSueldo = new System.Windows.Forms.Label();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnRegistrar = new System.Windows.Forms.Button();
+            this.lvEmpleados = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label6 = new System.Windows.Forms.Label();
+            this.lvEstadisticas = new System.Windows.Forms.ListView();
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader8 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader9 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Nobile", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Tomato;
+            this.label1.Location = new System.Drawing.Point(173, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(343, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "PLANILLA DE SUELDOS";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(31, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "FECHA";
+            // 
+            // lblFecha
+            // 
+            this.lblFecha.AutoSize = true;
+            this.lblFecha.BackColor = System.Drawing.Color.OrangeRed;
+            this.lblFecha.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFecha.ForeColor = System.Drawing.Color.MistyRose;
+            this.lblFecha.Location = new System.Drawing.Point(30, 72);
+            this.lblFecha.Name = "lblFecha";
+            this.lblFecha.Size = new System.Drawing.Size(77, 20);
+            this.lblFecha.TabIndex = 2;
+            this.lblFecha.Text = "lblFecha";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Tomato;
+            this.label3.Location = new System.Drawing.Point(31, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "DNI";
+            // 
+            // txtDNI
+            // 
+            this.txtDNI.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDNI.ForeColor = System.Drawing.Color.OrangeRed;
+            this.txtDNI.Location = new System.Drawing.Point(34, 136);
+            this.txtDNI.MaxLength = 18;
+            this.txtDNI.Name = "txtDNI";
+            this.txtDNI.Size = new System.Drawing.Size(161, 26);
+            this.txtDNI.TabIndex = 4;
+            // 
+            // txtEmpleado
+            // 
+            this.txtEmpleado.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmpleado.ForeColor = System.Drawing.Color.OrangeRed;
+            this.txtEmpleado.Location = new System.Drawing.Point(212, 136);
+            this.txtEmpleado.MaxLength = 250;
+            this.txtEmpleado.Name = "txtEmpleado";
+            this.txtEmpleado.Size = new System.Drawing.Size(429, 26);
+            this.txtEmpleado.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Tomato;
+            this.label4.Location = new System.Drawing.Point(209, 117);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 16);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "EMPLEADO";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Tomato;
+            this.label5.Location = new System.Drawing.Point(31, 181);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(94, 16);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "CATEGORÍA";
+            // 
+            // cboCategoria
+            // 
+            this.cboCategoria.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboCategoria.ForeColor = System.Drawing.Color.OrangeRed;
+            this.cboCategoria.FormattingEnabled = true;
+            this.cboCategoria.Items.AddRange(new object[] {
+            "Jefe",
+            "Supervisor",
+            "Vendedor",
+            "Administrador"});
+            this.cboCategoria.Location = new System.Drawing.Point(34, 200);
+            this.cboCategoria.Name = "cboCategoria";
+            this.cboCategoria.Size = new System.Drawing.Size(161, 28);
+            this.cboCategoria.TabIndex = 8;
+            this.cboCategoria.SelectedIndexChanged += new System.EventHandler(this.cboCategoria_SelectedIndexChanged);
+            // 
+            // lblSueldo
+            // 
+            this.lblSueldo.AutoSize = true;
+            this.lblSueldo.BackColor = System.Drawing.Color.OrangeRed;
+            this.lblSueldo.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblSueldo.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSueldo.ForeColor = System.Drawing.Color.MistyRose;
+            this.lblSueldo.Location = new System.Drawing.Point(240, 193);
+            this.lblSueldo.Name = "lblSueldo";
+            this.lblSueldo.Size = new System.Drawing.Size(94, 33);
+            this.lblSueldo.TabIndex = 9;
+            this.lblSueldo.Text = "label6";
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btnCancelar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelar.ForeColor = System.Drawing.Color.SeaShell;
+            this.btnCancelar.Location = new System.Drawing.Point(506, 182);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(135, 35);
+            this.btnCancelar.TabIndex = 10;
+            this.btnCancelar.Text = "CANCELAR";
+            this.btnCancelar.UseVisualStyleBackColor = false;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // btnRegistrar
+            // 
+            this.btnRegistrar.BackColor = System.Drawing.Color.OrangeRed;
+            this.btnRegistrar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnRegistrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegistrar.ForeColor = System.Drawing.Color.MistyRose;
+            this.btnRegistrar.Location = new System.Drawing.Point(506, 223);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(135, 35);
+            this.btnRegistrar.TabIndex = 11;
+            this.btnRegistrar.Text = "REGISTRAR";
+            this.btnRegistrar.UseVisualStyleBackColor = false;
+            this.btnRegistrar.Click += new System.EventHandler(this.btnRegistrar_Click);
+            // 
+            // lvEmpleados
+            // 
+            this.lvEmpleados.BackColor = System.Drawing.Color.OrangeRed;
+            this.lvEmpleados.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.lvEmpleados.ForeColor = System.Drawing.Color.MistyRose;
+            this.lvEmpleados.GridLines = true;
+            this.lvEmpleados.HideSelection = false;
+            this.lvEmpleados.Location = new System.Drawing.Point(34, 264);
+            this.lvEmpleados.Name = "lvEmpleados";
+            this.lvEmpleados.Size = new System.Drawing.Size(607, 127);
+            this.lvEmpleados.TabIndex = 12;
+            this.lvEmpleados.UseCompatibleStateImageBehavior = false;
+            this.lvEmpleados.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "DNI";
+            this.columnHeader1.Width = 150;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "EMPLEADO";
+            this.columnHeader2.Width = 200;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "CATEGORIA";
+            this.columnHeader3.Width = 130;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "SUELDO";
+            this.columnHeader4.Width = 120;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.OrangeRed;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.MistyRose;
+            this.label6.Location = new System.Drawing.Point(276, 414);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(138, 20);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "ESTADISTICAS";
+            // 
+            // lvEstadisticas
+            // 
+            this.lvEstadisticas.BackColor = System.Drawing.Color.MistyRose;
+            this.lvEstadisticas.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader7,
+            this.columnHeader8,
+            this.columnHeader9});
+            this.lvEstadisticas.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lvEstadisticas.GridLines = true;
+            this.lvEstadisticas.HideSelection = false;
+            this.lvEstadisticas.Location = new System.Drawing.Point(34, 437);
+            this.lvEstadisticas.Name = "lvEstadisticas";
+            this.lvEstadisticas.Size = new System.Drawing.Size(607, 127);
+            this.lvEstadisticas.TabIndex = 14;
+            this.lvEstadisticas.UseCompatibleStateImageBehavior = false;
+            this.lvEstadisticas.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "CATEGORIA";
+            this.columnHeader7.Width = 300;
+            // 
+            // columnHeader8
+            // 
+            this.columnHeader8.Text = "TOTAL";
+            this.columnHeader8.Width = 150;
+            // 
+            // columnHeader9
+            // 
+            this.columnHeader9.Text = "TOTAL SUELDO";
+            this.columnHeader9.Width = 150;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btnSalir.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSalir.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.ForeColor = System.Drawing.Color.SeaShell;
+            this.btnSalir.Location = new System.Drawing.Point(34, 578);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(135, 35);
+            this.btnSalir.TabIndex = 15;
+            this.btnSalir.Text = "SALIR";
+            this.btnSalir.UseVisualStyleBackColor = false;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // frmPlanilla
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MistyRose;
+            this.ClientSize = new System.Drawing.Size(681, 625);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.lvEstadisticas);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.lvEmpleados);
+            this.Controls.Add(this.btnRegistrar);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.lblSueldo);
+            this.Controls.Add(this.cboCategoria);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtEmpleado);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtDNI);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblFecha);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.OrangeRed;
+            this.Name = "frmPlanilla";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Planilla de Sueldos";
+            this.Load += new System.EventHandler(this.frmPlanilla_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblFecha;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtDNI;
+        private System.Windows.Forms.TextBox txtEmpleado;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cboCategoria;
+        private System.Windows.Forms.Label lblSueldo;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnRegistrar;
+        private System.Windows.Forms.ListView lvEmpleados;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ListView lvEstadisticas;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
+        private System.Windows.Forms.ColumnHeader columnHeader8;
+        private System.Windows.Forms.ColumnHeader columnHeader9;
+        private System.Windows.Forms.Button btnSalir;
+    }
+}
+
---
+++ P18_Planilla_Sueldo/frmPlanilla.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+// Angie Yahaira Campos Arias
+// 20222000215
+
+namespace P18_Planilla_Sueldo
+{
+    public partial class frmPlanilla : Form
+    {
+        double saldo;
+        int cJefe, cSupervisor, cVendedor, cAdministrador;
+        double aJefe, aSupervisor, aVendedor, aAdministrador;
+
+        
+
+        public frmPlanilla()
+        {
+            InitializeComponent();
+        }
+        private void frmPlanilla_Load(object sender, EventArgs e)
+        {
+            lblFecha.Text = DateTime.Now.ToString("d");
+            lblSueldo.Text = (0).ToString("C");
+        }
+
+        private void cboCategoria_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string categoria = cboCategoria.Text;
+
+            switch (categoria)
+            {
+                case "Jefe": saldo = 5000; break;
+                case "Supervisor": saldo = 3500; break;
+                case "Vendedor": saldo = 1500; break;
+                case "Administrador": saldo = 7000; break;
+            }
+
+            lblSueldo.Text = saldo.ToString("C");
+        }
+
+        private void btnRegistrar_Click(object sender, EventArgs e)
+        {
+            string DNI = txtDNI.Text;
+            string empleado = txtEmpleado.Text;
+            string categoria = cboCategoria.Text;
+
+            switch (categoria)
+            {
+                case "Jefe":
+                    cJefe++;
+                    aJefe += saldo;
+                    break;
+                case "Supervisor":
+                    cSupervisor++;
+                    aSupervisor += saldo;
+                    break;
+                case "Vendedor":
+                    cVendedor++;
+                    aVendedor += saldo;
+                    break;
+                case "Administrador":
+                    cAdministrador++;
+                    aAdministrador += saldo;
+                    break;
+            }
+
+            ListViewItem fila = new ListViewItem(DNI);
+            fila.SubItems.Add(empleado);
+            fila.SubItems.Add(categoria);
+            fila.SubItems.Add(saldo.ToString("C"));
+            lvEmpleados.Items.Add(fila);
+
+            lvEstadisticas.Items.Clear();
+            string[] elementosFila = new string[3];
+            ListViewItem row;
+
+            elementosFila[0] = "Jefe";
+            elementosFila[1] = cJefe.ToString();
+            elementosFila[2] = aJefe.ToString("C");
+            row = new ListViewItem(elementosFila);
+            lvEstadisticas.Items.Add(row);
+
+            elementosFila[0] = "Supervisor";
+            elementosFila[1] = cSupervisor.ToString();
+            elementosFila[2] = aSupervisor.ToString("C");
+            row = new ListViewItem(elementosFila);
+            lvEstadisticas.Items.Add(row);
+
+            elementosFila[0] = "Vendedor";
+            elementosFila[1] = cVendedor.ToString();
+            elementosFila[2] = aVendedor.ToString("C");
+            row = new ListViewItem(elementosFila);
+            lvEstadisticas.Items.Add(row);
+
+            elementosFila[0] = "Administrador";
+            elementosFila[1] = cAdministrador.ToString();
+            elementosFila[2] = aAdministrador.ToString("C");
+            row = new ListViewItem(elementosFila);
+            lvEstadisticas.Items.Add(row);
+
+            txtDNI.Clear();
+            txtEmpleado.Clear();
+            cboCategoria.SelectedIndex = -1;
+            lblSueldo.Text = (0).ToString("C");
+            txtEmpleado.Focus();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            DialogResult r = MessageBox.Show("Esta seguro de salir?", "Planilla",
+                                            MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (r == DialogResult.Yes) this.Close();
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            txtDNI.Clear();
+            txtEmpleado.Clear();
+            cboCategoria.SelectedIndex = -1;
+            lblSueldo.Text = (0).ToString("C");
+            txtEmpleado.Focus();
+        }
+    }
+}
